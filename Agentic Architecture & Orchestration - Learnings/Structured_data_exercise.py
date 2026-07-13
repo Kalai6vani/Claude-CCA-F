26f1ea33dd507ddcaa2498b7620d1304b791a6ff
Kalai6vani <90702110+Kalai6vani@users.noreply.github.com>
2026-07-13 13:12:29 +0530

    Implement multi-turn conversation helper functions
    Added helper functions for multi-turn conversation management and updated chat function to handle messages.

--- Agentic Architecture & Orchestration - Learnings/Structured_data_exercise.py
+++ Agentic Architecture & Orchestration - Learnings/Structured_data_exercise.py
@@ -1,5 +1,28 @@
+#Helper functions to maintain MULTI - TURN CONVERSATION
+def add_user_message(messages, text):
+  user_message = {"role": "user", "content": text}
+  messages.append(user_message)
+
+def add_assistant_message(messages, text):
+  assistant_message = {"role": "assistant", "content": text}
+  messages.append(assistant_message)
+
+def chat(messages):
+  message = client.messages.create(
+    model=model,
+    max_tokens=1000,
+    messages=messages,
+  )
+  return message.content[0].text
+
+#------------------------------------main-------------------------------------
 messages = []
 
 add_user_message(messages, "Generate a very short event bridge rule as json")
+add_assistant_message(messages, "```json")
 
-chat(messages)
+text = chat(messages, stop_sequences=["```"])
+text
+
+#import json
+#json.load(text.strip())
